401ce8ee18c3419131e5f4b1ee199ab232d54410
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-10-29 20:34:59 +0700

    create src directory

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ CC=${CC:-gcc}
 
 CFLAGS="-std=c++11 -O2 -g3 -Wall -Wextra -fno-stack-protector -ffreestanding -nostdlib -no-pie"
 
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.cpp
+$CC $CFLAGS -Isrc -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    src/kernel.cpp
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -kernel kernel.elf
