bf663bcd5e81e1079d732bb505e2480912aa08f9
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-11-10 21:20:26 +0900

    application

--- run.sh
+++ run.sh
@@ -7,6 +7,12 @@ CC=${CC:-gcc}
 
 CFLAGS="-std=c++11 -O2 -g3 -Wall -Wextra -fno-stack-protector -ffreestanding -nostdlib -no-pie"
 
+OBJCOPY=${OBJCOPY:-riscv32-none-elf-objcopy}
+
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf src/shell.cpp src/user.cpp src/common.cpp
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 $CC $CFLAGS -Isrc -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     src/kernel.cpp src/common.cpp
 
---
+++ src/user.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "common.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    __attribute__((noreturn)) void exit(void);
+    void putchar(char ch);
+
+#ifdef __cplusplus
+}
+#endif
