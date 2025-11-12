f70805c4e5f801361bca221c60c435cbcbff21ed
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-11-12 20:59:26 +0900

    disk io

--- run.sh
+++ run.sh
@@ -17,4 +17,7 @@ $CC $CFLAGS -Isrc -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     src/kernel.cpp src/common.cpp shell.bin.o
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+    -drive id=drive0,file=lorem.txt,format=raw,if=none \
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
     -kernel kernel.elf
--- src/common.h
+++ src/common.h
@@ -11,7 +11,7 @@ typedef uint32_t vaddr_t;
 #ifdef __cplusplus
 #define NULL nullptr
 #endif
-#define align_up(value, align) __builtin_align_up(value, align)
+#define align_up(value, align) (((value) + (align) - 1) & ~((align) - 1))
 #define is_aligned(value, align) (((value) & ((align) - 1)) == 0)
 #define offsetof(type, member) __builtin_offsetof(type, member)
 #define va_list __builtin_va_list
